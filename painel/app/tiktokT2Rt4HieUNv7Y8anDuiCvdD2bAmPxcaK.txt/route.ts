b460bf81d6af609190eb6ed3ed9421675b5a3528
Andre Duarte <andrelgduarte@gmail.com>
2026-06-29 11:39:19 -0300

    TikTok verify: troca arquivo estático por route handler

---
+++ painel/app/tiktokT2Rt4HieUNv7Y8anDuiCvdD2bAmPxcaK.txt/route.ts
@@ -0,0 +1,7 @@
+export const dynamic = "force-dynamic";
+
+export async function GET() {
+  return new Response("tiktokT2Rt4HieUNv7Y8anDuiCvdD2bAmPxcaK", {
+    headers: { "content-type": "text/plain; charset=utf-8" },
+  });
+}
